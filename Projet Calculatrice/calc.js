c2dcc3ca0692a0aae20a4bc04bbb04bf2edac43f
Léo GUISLAIN <104099039+Yukihirooh@users.noreply.github.com>
2022-06-20 16:09:51 +0200

    add security

--- Projet Calculatrice/calc.js
+++ Projet Calculatrice/calc.js
@@ -1,5 +1,7 @@
 let result = document.querySelector(".result")
 let chiffre = document.querySelectorAll(".chiffre")
+let operator = document.querySelectorAll(".operator")
+let virgule = document.querySelector(".virgule")
 
 document.querySelector(".boutonegal").addEventListener("click", function () {
     result.innerHTML = calcul(document.querySelector(".result").textContent);
@@ -17,6 +19,24 @@ chiffre.forEach(function (i) {
     i.addEventListener('click', function () {
         document.querySelector('.result').innerHTML += i.innerHTML
     })
+})
+
+operator.forEach(function (i) {
+    i.addEventListener('click', function () {
+        if (result.innerHTML === "") {
+            return
+        } else {
+            document.querySelector('.result').innerHTML += i.innerHTML
+        }
+    })
+})
+
+virgule.addEventListener("click", function (){
+    if (result.innerHTML === "") {
+        result.innerHTML += "0."
+    } else {
+        result.innerHTML += "."
+    }
 })
 
 function calcul(obj) {
